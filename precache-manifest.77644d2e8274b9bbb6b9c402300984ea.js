8ccb171e169d9f44df500316ef2ba6ed2ffddf53
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2019-02-23 17:24:32 -0300

    Updates

---
+++ precache-manifest.77644d2e8274b9bbb6b9c402300984ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7af2887a4999986d472",
+    "url": "/gustavoteodoro/static/js/runtime~main.a7af2887.js"
+  },
+  {
+    "revision": "82155d8444de61531cf0",
+    "url": "/gustavoteodoro/static/js/main.82155d84.chunk.js"
+  },
+  {
+    "revision": "9563c54a32f25a533831",
+    "url": "/gustavoteodoro/static/js/2.9563c54a.chunk.js"
+  },
+  {
+    "revision": "82155d8444de61531cf0",
+    "url": "/gustavoteodoro/static/css/main.9a1f29c7.chunk.css"
+  },
+  {
+    "revision": "2f2c124fb1c307843d64b8a50029a1c0",
+    "url": "/gustavoteodoro/index.html"
+  }
+];
